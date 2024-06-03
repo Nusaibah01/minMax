26977be2b08165570a5ee55e26de95d5878eee09
Nusaibah01 <charifnusaibah01@gmail.com>
2024-06-03 23:03:34 +0200

    codejs

---
+++ script/code.js
@@ -0,0 +1,15 @@
+// Displaying the minimum and maximum numbers within an array
+let numbers = ["2", "4", "6", "8","10" , "12"]
+
+
+let min = Math.min(...numbers)
+console.log(min)
+let max = Math.max(...numbers)
+console.log(max)
+
+
+
+
+
+
+
